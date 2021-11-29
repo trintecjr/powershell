b6666090674f9fc46f09b324c82f9aa2795b6aad
Gerson Junior <51932106+trintecjr@users.noreply.github.com>
2021-11-29 16:50:47 -0300

    Add files via upload

---
+++ Exemplo_Looping_For.ps1
@@ -0,0 +1,13 @@
+﻿# Exemplo Looping For
+Clear-Host
+# 1 é Menor ou Igual á 10 ( -le ) então acrescente 1 ( ++ )
+# For (inicio; condição; proximo valor) {Código de repetição}
+For ($Var=1; $var -le 10; $Var++)
+# Mostrar o Looping
+{
+Test-Connection 192.168.0.$Var
+}
+# Testar em Looping
+{
+Test-Connection 192.168.0.$Var
+}
